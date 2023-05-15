96a148b5dac4bf2c2fec9185c5131468df2b31b6
HaoHan <haohan11pfer@protonmail.com>
2023-05-15 08:44:03 +0800

    Add edit name feature

--- api/c_l_handle_edit.php
+++ api/c_l_handle_edit.php
@@ -2,8 +2,6 @@
     include '../parts/admin-required.php' ;
     include '../parts/db-connect.php' ;
 
-    $table_name = 'c_l_coach' ;
-
     $output = [
         'success' => false,
         'post' => $_POST,
@@ -11,31 +9,60 @@
         'error' => []
     ];
 
-    if(empty($_FILES['data'])) quit() ;
+    $type_gate = [
+        'photo' => 'EditPhoto',
+        'name' => 'EditName',
+    ] ;
+
+    if(empty($_POST['type'])) quit() ;
+
+    $type_gate[$_POST['type']]() ;
+
+
+    function EditName() {
+        global $pdo, $output ;
+
+        if(empty($_POST['data'])) quit() ;
+
+        $sql = "UPDATE `member` SET `{$_POST['type']}` = '{$_POST['data']}' 
+                WHERE `sid` = ( SELECT `member_sid` FROM `c_l_coach` 
+                WHERE `sid` = {$_POST['sid']} )"
+        ;
+
+        $statment = $pdo->query($sql) ;
     
-    list( $file_type, $file_extension ) = explode("/", $_FILES['data']['type'] ) ;
+        $output['success'] = !! $statment->rowCount() ;
+    
+        quit() ;
+    }
 
-    if($file_type !== 'image') quit() ;
+    function EditPhoto() {
+        global $pdo, $output ;
 
-    $result = saveImage() ;
-
-    if(!$result) quit('fail to move file') ;
-
-    $sql = "UPDATE `{$table_name}`
-            SET `{$_POST['type']}` = '{$result}' 
-            WHERE `sid` = {$_POST['sid']}"
-    ;
-
-    $statment = $pdo->query($sql) ;
-
-    $output['success'] = !! $statment->rowCount() ;
-
-    quit() ;
-
-    // ----------------- function declare blow -----------------
+        if(empty($_FILES['data'])) quit() ;
+        
+        $file_type = explode("/", $_FILES['data']['type'] )[0] ;
+    
+        if($file_type !== 'image') quit() ;
+    
+        $result = saveImage() ;
+    
+        if(!$result) quit('fail to move file') ;
+    
+        $sql = "UPDATE `c_l_coach`
+                SET `{$_POST['type']}` = '{$result}' 
+                WHERE `sid` = {$_POST['sid']}"
+        ;
+    
+        $statment = $pdo->query($sql) ;
+    
+        $output['success'] = !! $statment->rowCount() ;
+    
+        quit() ;
+    }
 
     function saveImage() {
-        global $file_extension ;
+        $file_extension = end(explode("/", $_FILES['data']['type'] )) ;
 
         $file_name = sha1( $_FILES['data']['name'] . uniqid() ) ;
         $save_path = "./imgs/coach_imgs/{$file_name}" ;
--- coach_list.php
+++ coach_list.php
@@ -8,33 +8,39 @@
 
 <style>
 
-    * {
+    .c_li_container * {
         box-sizing: border-box;
     }
 
-    .c_l_container .card{
-        /* max-height: 100px; */
+    .c_li_container .card{
         padding: 20px;
+        min-width: 200px;
         height: 220px;
     }
 
-    .c_l_container .img-box {
+    @media (max-width: 767px) {
+        .c_li_container .card{
+            height: 100%;
+        }
+    }
+
+    .c_li_container .img-box {
         position: relative;
         height: 100%;
     }
 
-    .c_l_container .img-box img {
+    .c_li_container .img-box img {
         object-fit: cover;
         height: 100%;
         width: 100%;
     }
 
-    .c_l_container .edit-img {
+    .c_li_container .edit-img {
         position: absolute;
         display: flex;
         justify-content: center;
         align-items: center;
-        font-size: 450%;
+        font-size: 4.5rem;
         height: 100%;
         width: 100%;
         top: 0;
@@ -45,18 +51,11 @@
         cursor: pointer;
     }
     
-    .c_l_container .edit-img:hover {
+    .c_li_container .edit-img:hover {
         opacity: 0.5;
     }
 
-    .c_l_container .card-body p {
-        padding: 2px 0;
-        margin: 2px 0;
-    }
-
-    .c_l_container .card-body .expereince {
-        font-style: oblique;
-    }
+    /* style for Swal below*/
 
     .c_l_spinner {
         width: 5rem !important;
@@ -67,22 +66,72 @@
         height: 1rem;
     }
 
+    /* style for Swal above*/
+
+    @media (max-width: 1340px) {
+        .c_li_container .coach_name_field {
+            flex-direction: column;
+        }
+
+        .c_li_container .coach_name_field .edit_field:last-child {
+            margin: 0 !important;
+        } 
+    }
+
+    .c_li_container .coach_name_field {
+        display: flex;
+        justify-content: start;
+    }
+
+
+    .c_li_container .edit_button {
+        all: unset;
+        cursor: pointer;
+    }
+
+    .c_li_container .edit_field {
+        font-size: 1.2rem;
+    }
+
+    .c_li_container .name_input {
+        position: absolute;
+        line-height: 1;
+        border-radius: 3px;
+        top: 0;
+        left: 0;
+        width: 6rem;
+        transition: width .5s, border-width 0s .5s;
+    }
+    
+    .c_li_container .hide {
+        border-width: 0;
+        padding: 0;
+        width: 0;
+    }
+
+    .c_li_container .hide_text {
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        overflow: hidden;
+    }
+
+
 </style>
 
 <?php
 
-    $sql = "SELECT * FROM `c_l_coach`" ;
+    $sql = "SELECT coach.*, member.name FROM `c_l_coach` AS coach JOIN `member` AS member WHERE coach.member_sid = member.sid" ;
     $statment = $pdo->query($sql) ;
     $rows = $statment->fetchAll() ;
 
 ?>
 
-<div class="c_l_container container">
+<div class="c_li_container container">
 
     <?php foreach($rows as $row): ?>
     <div class="card mb-3">
         <div class="row g-0 h-100">
-            <div class="col-md-2 img-box">
+            <div class="col-lg-2 col-md-3 col-sm-4 img-box">
                 <img src="<?= $row['photo'] ?>" class="img-fluid rounded" alt="...">
                 <i class="fa-solid fa-pen-to-square edit-img" onclick="document.getElementById('photo_<?= $row['sid'] ?>').click()"></i>
                 <input type="file" accept="image/png, image/jpeg, image/webp" id="photo_<?= $row['sid'] ?>" onchange="edit({
@@ -91,11 +140,61 @@
                     'data': this.files[0]
                 })" hidden>
             </div>
-            <div class="col-md-10">
-                <div class="card-body">
-                    <h5 class="card-title"><?= $row['nickname'] ?></h5>
-                    <p class="card-text expereince"><?= $row['experience'] ?></p>
-                    <p class="card-text"><?= $row['introduction'] ?></p>
+            <div class="col-lg-10 col-md-9 col-sm-8">
+                <div class="card-body py-0">
+                    <div class="coach_name_field">
+                        <?php 
+                            $list = ['name','nickname'] ;
+                            foreach($list as $label) :
+                        ?>
+                        <span class="edit_field me-3">
+                            <label for="" class="me-1 fw-bold">
+                                <?= $label == 'name' ? '姓名' : '暱稱' ?>:
+                            </label>
+                            <button class="edit_button position-relative align-middle" type="button" onclick="
+                                const inputEl = this.querySelector('input');
+                                const spanEl = this.querySelector('span');
+                                inputEl.classList.remove('hide');
+                                inputEl.focus();
+                                inputEl.value = spanEl.textContent;
+                                inputEl.addEventListener('blur', () => {
+                                    inputEl.classList.add('hide');
+                                    inputEl.value = '';
+                                }, {'once': true});
+                            ">
+                                <span class="d-inline-block hide_text" style="width: 6rem;  line-height: 1.2"><?= $row[$label] ?></span>
+                                <input class="name_input hide" type="text" onkeyup="
+                                    if(event.which !== 13) return; 
+                                    edit({  'sid': <?= $row['sid'] ?>,
+                                            'type': '<?= $label ?>',
+                                            'data': this.value
+                                    }); 
+                                    this.blur();
+                                ">
+                                <i class="fa-solid fa-pen-to-square ms-1"></i>
+                            </button>
+                        </span>
+                        <?php endforeach ; ?>
+
+                        <span class="edit_field ms-auto">
+                            <label for="" class="me-1 fw-bold">加入時間:</label>
+                            <button class="edit_button" type="button" onclick="">
+                                <span><?= explode(' ',$row['created_at'])[0] ?></span>
+                                <i class="fa-solid fa-pen-to-square ms-1"></i>
+                            </button>
+                            <input type="text" hidden>
+                        </span>
+                    </div>
+                    <div class="row edit_field">
+                        <div class="col-1 lh-lg fw-bold pe-0">經歷:</div>
+                        <div class="col-10 lh-lg hide_text"><?= $row['experience'] ?></div>
+                        <div class="col-1 text-end"><i class="fa-solid fa-pen-to-square ms-1"></i></div>
+                    </div>
+                    <div class="row edit_field">
+                        <div class="col-1 lh-lg fw-bold pe-0">介紹:</div>
+                        <div class="col-10 lh-lg hide_text"><?= $row['introduction'] ?></div>
+                        <div class="col-1 text-end"><i class="fa-solid fa-pen-to-square ms-1"></i></div>
+                    </div>
                 </div>
             </div>
         </div>
