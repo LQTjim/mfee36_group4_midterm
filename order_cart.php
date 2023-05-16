92ab74aacc08321175d2211e2e927cd16072f5f6
shihu <miraculous0218@hotmail.com>
2023-05-16 17:21:58 +0800

    cart edit

--- order_cart.php
+++ order_cart.php
@@ -5,9 +5,7 @@ $title = 'Cart'
 <?php include "./parts/db-connect.php"; ?>
 
 <?php
-// include './connectdb.php';
-// include './order_select.php';
-$perPage = 10; #每頁幾筆資料
+$perPage = 20; #每頁幾筆資料
 $totolRows = //抓總筆數
     "SELECT
     count(*)
@@ -15,17 +13,15 @@ FROM
     order_cart
 ";
 $totalRowsNum = $pdo->query($totolRows)->fetch(PDO::FETCH_NUM)[0];
-// echo json_encode($totalPageNum);
-// exit;
 $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$totalPage = ceil($totalRowsNum / $perPage);
+
 if ($page < 1) {
     header('location: ?page=1');
     exit;
 };
 
-
 if ($totalRowsNum) :
-    $totalPage = ceil($totalRowsNum / $perPage);
     if ($page > $totalPage) :
         header("location:?page=$totalPage");
     endif;
@@ -51,10 +47,6 @@ LIMIT
     $stmt_order_cart = $pdo->query($sql_order_cart);
     $rows_order_cart = $stmt_order_cart->fetchAll();
 endif;
-
-
-
-
 ?>
 
 <?php include "./parts/admin-required.php"; ?>
@@ -62,6 +54,7 @@ endif;
 <?php include "./parts/html-navbar.php"; ?>
 
 <div class="container">
+    <!-- 上方分頁紐 -->
     <div class="row">
         <nav aria-label="Page navigation example">
             <ul class="pagination">
@@ -98,10 +91,13 @@ endif;
             </ul>
         </nav>
     </div>
+
+    <!-- 主介面 -->
     <div class="row">
         <table class="table">
             <thead>
                 <tr>
+                    <th scope="delete">編輯</th>
                     <th scope="col">編號</th>
                     <th scope="col">會員編號</th>
                     <th scope="col">會員姓名</th>
@@ -110,12 +106,14 @@ endif;
                     <th scope="col">數量</th>
                     <th scope="col">小計(NTD)</th>
                     <th scope="col">加入購物車時間</th>
+                    <th scope="delete">刪除</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($rows_order_cart as $r) : ?>
                     <tr>
-                        <th scope="row"><?= $r['sid'] ?></th>
+                        <td><a href=""><i class="fa-solid fa-pen-to-square"></i></a></td>
+                        <td><?= $r['sid'] ?></td>
                         <td><?= $r['member_sid'] ?></td>
                         <td><?= $r['name'] ?></td>
                         <td><?= $r['product_type'] ?></td>
@@ -123,11 +121,48 @@ endif;
                         <td><?= $r['quantity'] ?></td>
                         <td><?= $r['amount'] ?></td>
                         <td><?= $r['created_at'] ?></td>
+                        <td><a href=""><i class="fa-solid fa-trash"></i></a></td>
+
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
     </div>
+    <!-- 下方分頁紐 -->
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+
+            <!-- page begining and page-1 -->
+            <li class="page-item"><a class="page-link <?= ($page == 1) ? 'disabled' : '' ?>" href="?page=<?= 1 ?>">
+                    <i class="fa-solid fa-angles-left"></i>
+                </a>
+            </li>
+            <li class="page-item"><a class="page-link <?= ($page == 1) ? 'disabled' : '' ?>" href=" ?page=<?= $page - 1 ?>">
+                    <i class="fa-solid fa-angle-left"></i></a>
+            </li>
+
+            <!-- page button -->
+            <?php for ($i = $page - 5; $i <= $page + 5; $i++) :
+                if ($i >= 1 and $i <= $totalPage) : ?>
+                    <li class="page-item <?= ($i == $page) ? 'active' : '' ?>">
+                        <?php if ($i == $page) : ?>
+                            <span class="page-link" href="?page=<?= $i ?>"><?= $i ?></span>
+                        <?php else : ?>
+                            <a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a>
+                        <?php endif; ?>
+                    </li>
+            <?php endif;
+            endfor ?>
+
+            <!-- page+1 and page end -->
+            <li class="page-item"><a class="page-link <?= ($page == $totalPage) ? 'disabled' : '' ?>" href="?page=<?= $page + 1 ?>">
+                    <i class="fa-solid fa-angle-right"></i></a>
+            </li>
+            <li class="page-item"><a class="page-link <?= ($page == $totalPage) ? 'disabled' : '' ?>" href="?page=<?= $totalPage ?>">
+                    <i class="fa-solid fa-angles-right"></i></a>
+            </li>
+        </ul>
+    </nav>
 </div>
 <script>
     // document.querySelector('#pageNow').removeAttribute('href');
