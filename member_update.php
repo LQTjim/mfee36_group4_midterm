38e9e33cf05d7c866bb341ccf828829ad5ddb997
LQTjim <jim6050482@hotmail.com>
2023-05-17 17:10:20 +0800

    update:update user finish

--- member_update.php
+++ member_update.php
@@ -35,20 +35,23 @@ if ($r == 0) {
     exit;
 }
 $r = $stmt->fetch();
-// if (COUNT($r) == 0) {
-//     header('Location: index.php');
-//     exit;
-// }
 ?>
 <link rel="stylesheet" href="./css/member.css">
 <div class="w-100 d-flex justify-content-center align-items-center">
+    <form name="form1" style="display: none">
+        <input type="file" name="hero_icon" id="avatar" accept="image/jpeg">
+    </form>
     <form class=" row w-100" name="update-member">
 
         <div class="border border-secondary rounded p-5 col">
             <div class="fs-3">修改會員</div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="text" name="email" class="form-control" id="email" value="<?= $r['email'] ?>">
+                <input disabled type="text" class="form-control" id="email" value="<?= $r['email'] ?>">
+            </div>
+            <div class="mb-3">
+                <label for="name" class="form-label">姓名</label>
+                <input type="text" name="name" class="form-control" id="name" value="<?= $r['name'] ?>">
             </div>
             <div class="mb-3">
                 <label for="mobile" class="form-label">電話</label>
@@ -60,14 +63,21 @@ $r = $stmt->fetch();
             </div>
             <div class="mb-3">
                 <label for="address" class="form-label">地址</label>
-                <input type="text" name="address" class="form-control" id="address" value="<?= $r['address'] ?>">
+                <select class="form-select d-inline" style="width:auto;" id="cityName"></select>
+                <select class="form-select d-inline" style="width:auto;" id="areaName">
+                    <option>請選擇</option>
+                </select>
+                <select class="form-select d-inline" style="width:auto;" id="roadName">
+                    <option>請選擇</option>
+                </select>
+                <input type="text" name="address" class="form-control mt-2" id="address" value="<?= $r['address'] ?>">
             </div>
             <div class="mb-3">
                 <label for="sex" class="form-label">性別</label>
                 <select class="form-select" name="sex" id="sex">
                     <option value="1" <?= $r['sex'] == 1 ? 'selected' : "" ?>>男</option>
                     <option value="2" <?= $r['sex'] == 2 ? 'selected' : "" ?>>女</option>
-                    <option value="3" <?= $r['sex'] == 3 ? 'selected' : "" ?>>不透漏</option>
+                    <option value="3" <?= $r['sex'] == 3 ? 'selected' : "" ?>>不透露</option>
                 </select>
             </div>
             <div class="mb-3">
@@ -89,29 +99,97 @@ $r = $stmt->fetch();
                 </select>
             </div>
             <div class="mb-3">
-                <label for="hero_icon" class="form-label">會員頭像</label>
-                <input type="file" name="hero_icon" id="hero_icon" />
+                <label for="active" class="form-label">啟用狀態</label>
+                <select class="form-select" name="active" id="active">
+                    <option value="0" <?= $r['active'] == 0 ? 'selected' : "" ?>>未啟用</option>
+                    <option value="1" <?= $r['active'] == 1 ? 'selected' : "" ?>>已啟用</option>
+                </select>
             </div>
+            <img class="member-update-img" src="<?= $r['hero_icon'] ?>" alt="">
+            <button type="button" class="btn btn-primary" onclick="selectFile()">上傳圖片</button>
         </div>
         <button type="button" class="btn btn-info text-secondary mt-2" id="updateBtn">確認送出</button>
     </form>
 </div>
+<script src="js/address.js"></script>
 <script>
+    const inp_avatar = document.querySelector('#avatar');
+    inp_avatar.addEventListener('change', function(event) {
+        event.preventDefault();
+        const fd = new FormData(document.form1);
+        fd.append('sid', <?= $_GET['sid'] ?>)
+        fetch('./api/member-list-update-icon-api.php', {
+                method: 'POST',
+                body: fd
+            }).then(r => r.json())
+            .then(obj => {
+                if (obj.filename) {
+                    document.querySelector('.member-update-img').src = './imgs/member_imgs/' + obj.filename;
+                }
+            })
+            .catch(ex => {
+                console.log(ex);
+            })
+    });
+
+    function selectFile() {
+        inp_avatar.click();
+    }
     (function() {
-        const form = document.querySelector('form');
+        const cityName = document.querySelector('#cityName');
+        const areaName = document.querySelector('#areaName');
+        const roadName = document.querySelector('#roadName');
+        const form = document.querySelector('form[name=update-member]');
         const updateBtn = document.querySelector('#updateBtn');
+        addressData.forEach((el) => {
+            cityName.innerHTML += `<option class="city-name">${el.CityName}</option>`
+        })
+        cityName.addEventListener('change', (e) => {
+            areaName.innerHTML = "";
+            const city = addressData.filter((el) => el.CityName === e.target.value)[0];
+            city.AreaList.forEach((el) => {
+                areaName.innerHTML += `<option class="city-name">${el.AreaName}</option>`
+            });
+        })
+        areaName.addEventListener('change', (e) => {
+            roadName.innerHTML = ""
+            const city = addressData.filter((el) => el.CityName === cityName.value)[0];
+            (city.AreaList.filter((el) => el.AreaName === e.target.value)[0]).RoadList.forEach((el) => {
+                roadName.innerHTML += `<option class="city-name">${el.RoadName}</option>`
+            })
+        });
+        roadName.addEventListener('change', () => {
+            document.querySelector("#address").value = ""
+            document.querySelector("#address").value = `${cityName.value}${areaName.value}${roadName.value}`
+        })
         updateBtn.addEventListener('click', () => {
             const fd = new FormData(form)
             fd.append('sid', <?= $_GET['sid'] ?>)
-            fetch('./api/member-list-update-api.php', {
-                method: "POST",
-                body: fd
-            }).then((r) => r.json()).then((d) => console.log(d)).catch((err) => {
-                console.log(err)
-            })
             for (var pair of fd.entries()) {
                 console.log(pair[0] + ', ' + pair[1]);
             }
+            fetch('./api/member-list-update-api.php', {
+                method: "POST",
+                body: fd
+            }).then((r) => r.json()).then(async (d) => {
+                console.log(d);
+                await Swal.fire({
+                    text: '修改成功',
+                    icon: 'success',
+                    timer: 1000,
+                    showCancelButton: false,
+                    showConfirmButton: false
+                })
+                location.reload();
+            }).catch((err) => {
+                console.log(err)
+                Swal.fire({
+                    text: '修改失敗，請聯絡工程師',
+                    icon: 'error',
+                    showCancelButton: false,
+                    showConfirmButton: false
+                })
+            })
         })
     })()
 </script>
