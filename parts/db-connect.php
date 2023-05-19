c849a8b1cc29477d96a63f50daa846fa3511befb
Sean <x90417@gmail.com>
2023-05-19 09:26:12 +0800

    midterm final

--- parts/db-connect.php
+++ parts/db-connect.php
@@ -2,9 +2,9 @@
 // require './parts/db-connect-local-config.php';
 $db_host = 'localhost';
 // $db_host = 'localhost:8889'; #TODO
-$db_name = 'final';
+$db_name = 'GYM_v2';
 $db_user = 'root';
-$db_pass = 'root';
+$db_pass = 'poop';
 $dsn = "mysql:host={$db_host};dbname={$db_name};charset=utf8mb4";
 $pdo_option = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
